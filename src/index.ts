23ff10ae5615bf20ef919eebd0648ea9b71ab531
Knut Egil <knut.egil.vn@gmail.com>
2024-02-14 18:44:23 +0100

    Fix minor stuff and setup database connection etc.

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+// Configure .env
+import dotenv from "dotenv";
+dotenv.config();
+
+// Import
+import express from "express";
+import cors from "cors";
+import database from "./database";
+
+// Async main wrapper
+async function main() {
+  try {
+    // Connect to database!
+    if (!(await database.init()))
+      throw new Error("Failed to establish database connection.");
+
+    // Success! Let's start our express app
+    console.info("Connected to database!");
+
+    // Create express app
+    const app = express();
+
+    // Set up middleware
+    app.use(express.json());
+    app.use(cors());
+
+    // Set up routes
+
+    // Listen
+    const PORT = process.env.PORT ?? 3000;
+    app.listen(PORT, () => {
+      console.info(`Server listening @ http://127.0.0.1:${PORT}`);
+    });
+  } catch (err) {
+    // Get error message
+    const { stack, message } = err as Error;
+    // Log error
+    console.error(`Something went wrong! Error: ${stack ?? message}`);
+  }
+}
+main();
