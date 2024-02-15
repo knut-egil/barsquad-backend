895db1ea21f3bd3b3916ac851b4001ccd4038191
Knut Egil <knut.egil.vn@gmail.com>
2024-02-15 23:20:28 +0100

    Add dummy squad /:code/ router with location route

--- src/routes/squad.router.ts
+++ src/routes/squad.router.ts
@@ -11,4 +11,24 @@ router.post("/create", create);
 // Join an existing squad by code
 router.post("/join", join);
 
+// Per-squad router
+const squadRouter = Router();
+
+// Update squad member location
+squadRouter.post("/location", (req, res) => {
+  // Log!
+  console.info(
+    `POST /:code/location, data: ${JSON.stringify(req.body, null, 2)}`
+  );
+
+  // TODO: Look up in squad registry
+  // and update location for the instance!
+
+  // Sure
+  res.end();
+});
+
+// Register per-squad router
+router.use("/:code", squadRouter);
+
 export default router;
